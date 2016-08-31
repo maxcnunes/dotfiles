bb094c08e266c29d1d4d39ff275ecba50aac5b3e
Max Claus Nunes <maxcnunes@gmail.com>
2016-08-31 13:17:34 -0300

    Configure Go install for linux

--- go/install.sh
+++ go/install.sh
@@ -7,12 +7,15 @@ source $dotfiles/script/helper.sh
 if [ "$OS" == "darwin" ]; then
   brew install go
 elif [ "$OS" == "linux" ]; then
-  echo "Not configured install for Golang"
+  GO_SRC="go1.7.linux-amd64.tar.gz"
+  cd /tmp
+  wget https://storage.googleapis.com/golang/$GO_SRC
+  tar -xf $GO_SRC
+  sudo mv go /usr/local
+  rm -rf $GO_SRC
 fi
 
-# gopath
-# # mkdir -p ~/go
-# # source go/path.sh
+mkdir -p ~/Development/go
 
 
 # $ go get golang.org/x/tools/cmd/godoc
