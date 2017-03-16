f3c94598063021bcaf9e3d067709fdc8eeb531b5
Max Claus Nunes <maxcnunes@gmail.com>
2017-03-16 15:02:02 -0300

    Fix brew install for caffeine

--- caffeine/install.sh
+++ caffeine/install.sh
@@ -5,7 +5,7 @@ dotfiles=$HOME/.dotfiles
 source $dotfiles/script/helper.sh
 
 if [ "$OS" == "darwin" ]; then
-  brew install caffeine
+  brew cask install caffeine
 elif [ "$OS" == "linux" ]; then
   sudo apt-get install -y caffeine
 fi
