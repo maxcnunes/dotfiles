42d19266959fb2daa100917c01221c03e7801023
Max Claus Nunes <maxcnunes@gmail.com>
2023-08-12 13:49:36 -0300

    Disable most formatting

--- nvim/lua/core/plugins/lsp/null-ls.lua
+++ nvim/lua/core/plugins/lsp/null-ls.lua
@@ -15,9 +15,9 @@ nls.setup {
     nls.builtins.diagnostics.golangci_lint, -- https://github.com/golangci/golangci-lint (~/.golangci.yml)
     nls.builtins.diagnostics.staticcheck, -- https://github.com/dominikh/go-tools
     -- nls.builtins.formatting.goimports_reviser, -- https://pkg.go.dev/github.com/incu6us/goimports-reviser
-    nls.builtins.formatting.goimports, -- https://pkg.go.dev/golang.org/x/tools/cmd/goimports
-    nls.builtins.formatting.shfmt, -- https://github.com/mvdan/sh
-    nls.builtins.formatting.gofumpt, -- https://github.com/mvdan/gofumpt
+    -- nls.builtins.formatting.goimports, -- https://pkg.go.dev/golang.org/x/tools/cmd/goimports
+    -- nls.builtins.formatting.shfmt, -- https://github.com/mvdan/sh
+    -- nls.builtins.formatting.gofumpt, -- https://github.com/mvdan/gofumpt
 
     -- Makefile
     nls.builtins.diagnostics.checkmake, -- https://github.com/mrtazz/checkmake
@@ -27,29 +27,29 @@ nls.setup {
     nls.builtins.diagnostics.write_good, -- https://github.com/btford/write-good
 
     -- Python
-    nls.builtins.formatting.black, -- https://github.com/psf/black
-    nls.builtins.formatting.autopep8, -- https://github.com/hhatto/autopep8
+    -- nls.builtins.formatting.black, -- https://github.com/psf/black
+    -- nls.builtins.formatting.autopep8, -- https://github.com/hhatto/autopep8
 
     -- JSON
-    nls.builtins.formatting.fixjson, -- https://github.com/rhysd/fixjson
+    -- nls.builtins.formatting.fixjson, -- https://github.com/rhysd/fixjson
 
-    nls.builtins.formatting.isort, -- https://github.com/PyCQA/isort
+    -- nls.builtins.formatting.isort, -- https://github.com/PyCQA/isort
 
     -- Markdown
-    nls.builtins.formatting.markdown_toc, -- https://github.com/jonschlinkert/markdown-toc
-    nls.builtins.formatting.mdformat, -- https://github.com/executablebooks/mdformat
+    -- nls.builtins.formatting.markdown_toc, -- https://github.com/jonschlinkert/markdown-toc
+    -- nls.builtins.formatting.mdformat, -- https://github.com/executablebooks/mdformat
 
     -- CHANGELOG.md
-    nls.builtins.formatting.ocdc, -- https://github.com/mdwint/ocdc
+    -- nls.builtins.formatting.ocdc, -- https://github.com/mdwint/ocdc
 
     -- TOML
-    nls.builtins.formatting.taplo, -- https://taplo.tamasfe.dev/
+    -- nls.builtins.formatting.taplo, -- https://taplo.tamasfe.dev/
 
     -- YAML
-    nls.builtins.formatting.yamlfmt, -- https://github.com/google/yamlfmt
+    -- nls.builtins.formatting.yamlfmt, -- https://github.com/google/yamlfmt
 
     -- Lua
-    nls.builtins.formatting.stylua, -- https://github.com/JohnnyMorganz/StyLua
+    -- nls.builtins.formatting.stylua, -- https://github.com/JohnnyMorganz/StyLua
 
     -- Javascript
     nls.builtins.diagnostics.eslint_d, -- https://github.com/mantoni/eslint_d.js/
@@ -60,7 +60,7 @@ nls.setup {
 
     -- Rust
     nls.builtins.diagnostics.ruff, -- https://github.com/astral-sh/ruff
-    nls.builtins.formatting.rustfmt, -- https://github.com/rust-lang/rustfmt
+    -- nls.builtins.formatting.rustfmt, -- https://github.com/rust-lang/rustfmt
   },
   on_attach = function(client, bufnr)
     vim.keymap.set('n', '<leader>tF', "<cmd>lua require('core.plugins.lsp.utils').toggle_autoformat()<cr>", { desc = 'Toggle format on save' })
